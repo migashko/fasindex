165846b9109c595133ea9c7d5dfb77fd283fd409
migashko <migashko@gmail.com>
2012-07-12 03:46:36 +0400

    upd

--- main.cpp
+++ main.cpp
@@ -1,16 +1,181 @@
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+       
 #include <functional>
 #include <vector>
 #include <algorithm>
 #include <stdexcept>
 #include <iostream>
+#include <bitset>
+#include <memory>
 
-template<typename T>
-class sorted_vector_iterator
+class mmap_manager
 {
 public:
-  typedef typename T::size_type size_type;
-  sorted_vector_iterator(T& t, size_type position)
+  ~mmap_manager()
+  {
+    munmap( _addr, _size );
+    close( _fd );
+  }
+  
+  mmap_manager(): _fd(-1), _addr(0), _size(0)
+  {
+  }
+  
+  bool open(const char* path, size_t size)
+  {
+    struct stat sb;
+    if ( -1 == stat(path, &sb) )
+      return false;
+    
+    _fd = ::open(path, O_RDWR | O_CREAT, (mode_t)0600);
+    
+    if ( _fd == -1)
+      return -1;
+    
+    if ( size > sb.st_size )
+    {
+      int lseek_result = lseek(_fd, size-1, SEEK_SET);
+      write(_fd, "", 1);
+    }
+    
+    _addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0);
+    
+    if (_addr == MAP_FAILED)
+    {
+      _addr = 0;
+      close( _fd );
+      _fd = -1;
+      return false;
+    }
+    
+    _size = size;
+    msync(_addr, _size, MS_SYNC);
+    return true;
+  }
+  
+  void resize(size_t size)
+  {
+    if (size > _size)
+    {
+      int lseek_result = lseek(_fd, size-1, SEEK_SET);
+      write(_fd, "", 1); 
+    }
+    else if (size < _size)
+    {
+      // TODO: урезать файл
+    }
+    _addr = mremap( _addr, _size, size, MREMAP_MAYMOVE);
+    _size = size;
+    if ( _addr == (void*)(-1) )
+      throw;
+  }
+  
+  size_t size() const { return _size; }
+  
+  void *addr() const { return _addr;}
+  
+  operator bool () const 
+  {
+    return _fd != -1;
+  }
+private:
+  int _fd;
+  void *_addr;
+  size_t _size;
+};
+
+template<typename T>
+struct mmap_allocator
+{
+  typedef T value_type;
+  typedef T* pointer;
+  typedef T& reference;
+  typedef const T* const_pointer;
+  typedef const T& const_reference;
+  typedef size_t size_type;
+  typedef std::ptrdiff_t difference_type;
+  
+  enum { block_size = sizeof(T)*sizeof(size_t) };
+  
+  mmap_manager& _mmm;
+  
+  mmap_allocator(mmap_manager& mmm): _mmm(mmm)
+  {
+  }
+  
+  
+  size_t restore(T*** result) const
+  {
+    if ( _mmm.size() < sizeof(size_t) )
+      return 0;
+    void *addr = _mmm.addr();
+    size_t size = *(reinterpret_cast<size_t*>(addr));
+    if (size == 0)
+      return 0;
+    *result = new T*[size];
+  }
+  
+  operator bool () const { return _mmm; }
+  
+private:
+ 
+};
+
+template<size_t N, typename Allocator = std::allocator<size_t> >
+class big_bitset
+{
+public:
+  typedef size_t size_type;
+  enum { bitset_count = N/sizeof(size_type) + (N%sizeof(size_type)!=0) };
+  
+  typedef Allocator allocator_type;
+  
+  ~big_bitset()
+  {
+    if (_bitset)
+      _allocator.deallocate(_bitset, bitset_count);
+  }
+  
+  big_bitset()
+  {
+    _bitset = _allocator.allocate(bitset_count);
+  }
+  
+  bool operator[] ( size_type i ) const { return _bitset[i/sizeof(size_type)] & (1 << i%sizeof(size_type)); }
+  
+  void set(size_type i)
+  {
+    _bitset[i/sizeof(size_type)] |= (1 << i%sizeof(size_type));
+  }
+  
+  allocator_type get_allocator() const
+  {
+    return _allocator;
+  }
+  
+private:
+  size_type* _bitset;
+  allocator_type _allocator;
+};
+
+template<typename ChainItr, typename CellItr>
+struct pair_iterator_t
+  : std::pair<ChainItr, CellItr>
+{
+  typedef std::pair<ChainItr, CellItr> super;
+  pair_iterator_t(){}
+  
+  pair_iterator_t(std::pair<ChainItr, CellItr> p): super(p) {}
+  
+  pair_iterator_t(ChainItr chain_itr, CellItr cell_itr )
+    : super(chain_itr, cell_itr)
   {}
+  
+  
 };
 
 template<typename K >
@@ -29,13 +194,16 @@ public:
   typedef std::vector<cell_type*> chain_type;
   typedef typename cell_type::iterator cell_iterator;
   typedef typename chain_type::iterator chain_iterator;
-  typedef std::pair<chain_iterator, cell_iterator> pair_iterator;
+  typedef pair_iterator_t<chain_iterator, cell_iterator> pair_iterator;
   typedef typename cell_type::size_type size_type;
-  typedef sorted_vector_iterator<self> iterator;
+  
+  /*typedef pair_iterator iterator;
   typedef sorted_vector_iterator<const self> const_iterator;
+  */
 
   sorted_vector(): _size(0)
   {
+    // TODO: при первой вставке 
     _chain.push_back(new cell_type);
   }
 
@@ -178,7 +346,31 @@ private:
 
 int main()
 {
+  mmap_manager mmm;
+  mmm.open("./mmap_manager.bin", 1111);
+  if (!mmm)
+    std::cout << "fuck" << std::endl;
+  //mmm.resize(5000);
+    
+    
+    
+  mmap_allocator<int> mma(mmm);
+  int **ii;
+  mma.restore(&ii);
+  return 0;
   
+  std::bitset<1024> myset;
+  big_bitset<1024> mybigset;
+  for (int i = 0; i < 1024; i++)
+    if ( i%11 == 0) mybigset.set(i);
+  for (int i = 0; i < 1024; i++)
+  {
+    if (i%64==0)
+      std::cout << std::endl;
+    std::cout << /*'0' + */mybigset[i] /*<< " "*/;
+  }
+  std::cout << std::endl;
+  /*
   typedef std::greater<int> comp;
   sorted_vector<int> v;
 
@@ -204,4 +396,5 @@ int main()
   for (int i =0 ; i < 20 ; ++i)
     std::cout << 0 + v[i] << std::endl;
   return 0;
+  */
 }
